db8dc5f7a5b952696cbaac5859f09745476476f2
Gautam Deo <117442395+gautamdeo0@users.noreply.github.com>
2025-09-10 08:54:50 +0530

    Add files via upload

---
+++ android/app/proguard-rules.pro
@@ -0,0 +1,1 @@
+-keep class com.example.healthsync_app.** { *; }
---
+++ android/app/src/main/kotlin/com/example/healthsync_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.healthsync_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
